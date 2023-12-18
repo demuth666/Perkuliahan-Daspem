0809dedc0b01aea567a3a7e88365b3c98258dcf3
demuth666 <eevnxxx05@gmail.com>
2023-12-18 11:40:21 +0700

    dasar pemrograman

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        queue.offer(2);
-        queue.offer(3);
-        queue.offer(4);
-        queue.offer(5);
-        queue.offer(6);
 
-        System.out.println("Queue: " + queue);
     }
 }
